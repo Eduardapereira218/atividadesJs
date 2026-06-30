506d96c779df980013a6b4e6beff0f00b9e40a3a
Eduarda <eduardapereiradossantos218@gmail.com>
2026-06-30 16:48:20 -0300

    Implement calculation and database insertion
    Add SQL insertion of calculation results into the database.

--- atividade17.php
+++ atividade17.php
@@ -1,4 +1,28 @@
+
 <?php 
+//incluir o arquivo responsavel  pela conexao
+
+include "conexao.php";
+
+$num1 = 20;
+$num2 = 23;
+
+$resultado = $num1 + $num2;
+
+//linguagem sql
+
+
+$sql = "INSERT INTO calculo(numero1, numero2, resultado)
+VALUES($num1, $num2, $resultado)";
+
+
+if ($conexao->query($sql)) {
+
+echo "<br> dados salvo com sucesso!";
+}else{
+    echo "<br> erro ao salvar";
+}
+
 
 
 ?>
